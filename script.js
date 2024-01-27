ac31e5f2fb41668dd95347734fc4689b43ebf9ca
rohitpednekar24 <pednekarrohit24@gmail.com>
2024-01-27 18:16:02 +0530

    First Commit

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ function toggleMenu(){
     icon.classList.toggle("open"); 
 
 }
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const filterButtons = document.querySelectorAll('.filter-btn');
+//     const projectContainers = document.querySelectorAll('.article-container');
+
+//     filterButtons.forEach(btn => {
+//         btn.addEventListener('click', function () {
+//             const category = this.getAttribute('data-category');
+//             filterProjects(category);
+//         });
+//     });
+
+//     function filterProjects(category) {
+//         projectContainers.forEach(container => {
+//             const containerCategory = container.getAttribute('data-category');
+//             if (category === 'all' || category === containerCategory) {
+//                 container.style.display = 'block';
+//             } else {
+//                 container.style.display = 'none';
+//             }
+//         });
+//     }
+// });
